a74530de1def4c67dea55abf521543f49ea031b0
Sandy <dinhduygiangvo@gmail.com>
2025-04-04 17:37:22 +0700

    Added new files
    Added new files

--- ADTCOURSE/WEEK 3 /Part 2 - Stacks/Course/LinkedLIst/Book.java
+++ ADTCOURSE/WEEK 3 /Part 2 - Stacks/Course/LinkedLIst/Book.java
@@ -1,5 +1,28 @@
 public class Book {
     private String bookTitle;
+    private Book   previousBook;
     private Book   nextBook;
     
+    public Book(String title) {
+        bookTitle = title;
+    }
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public Book getPreviousBook() {
+        return previousBook;
+    }
+
+    public Book getNextBook() {
+        return nextBook;
+    }
+
+    public void setNextBook(Book next) {
+        nextBook = next;
+    }
+
+    public void setPreviousBook(Book previous) {
+        previousBook = previous;
+    }
 }
--- ADTCOURSE/WEEK 3 /Part 2 - Stacks/Course/LinkedLIst/BookStackL.java
+++ ADTCOURSE/WEEK 3 /Part 2 - Stacks/Course/LinkedLIst/BookStackL.java
@@ -1,3 +1,63 @@
 public class BookStackL {
-    private Book head;   
+    private Book top;
+    private Book bottom;
+    private int numToRead = 0;
+    private int numRead = 0;
+
+    public BookStackL() {
+        top = new Book(null);
+        bottom = new Book(null);
+        top.setNextBook(bottom);
+        bottom.setPreviousBook(top);
+    }
+
+    private Book getBookAt(int index) {
+        Book get = top;
+        int i = 0;
+        while (i < index) {
+            get = get.getNextBook();
+            i++;
+        }
+        return get;
+    }
+    public void push(String title) {
+        Book bookPush = new Book(title);
+        Book bookAtIndex = getBookAt(numToRead);
+        Book bookUnder = bookAtIndex.getNextBook();
+        bookAtIndex.setNextBook(bookPush);
+        bookPush.setNextBook(bookUnder);
+        bookPush.setPreviousBook(bookAtIndex);
+        numToRead++;
+        System.out.println("Prev: " + bookPush.getPreviousBook().getBookTitle() + " | " + "Name: " + bookPush.getBookTitle() + " | " + "Post: " + bookPush.getNextBook().getBookTitle());
+    }
+
+    public boolean read() {
+        if (numToRead == 0) {
+            return false;
+        }
+        numToRead--;
+        numRead++;
+        return true;
+    }
+    public String toString() {
+        String stringForm = "";
+        stringForm += "Number of books to read: " + numToRead + ", " + "Number of books read: " + numRead + "\n";
+
+        Book getTop = top.getNextBook();
+        while (getTop != bottom) {
+            stringForm += "\"" + getTop.getBookTitle() + "\"" + " ";
+            getTop = getTop.getNextBook();
+        }
+        return stringForm;
+    }
+
+    public static void main(String[] args) {
+        BookStackL bookStack = new BookStackL();
+        bookStack.push("Book 1");
+        bookStack.push("Book 2");
+        bookStack.push("Book 3");
+        bookStack.read();
+        bookStack.push("Book 10");
+        System.out.println(bookStack.toString());
+    }
 }
